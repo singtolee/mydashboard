3134f88834810a6cb4d0aa7d557fac9f12fee369
singtolee <liqiang0618@outlook.com>
2018-08-01 14:13:04 +0700

    order page use accordion

--- src/app/edit-product/edit-product.component.ts
+++ src/app/edit-product/edit-product.component.ts
@@ -1,9 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFirestore } from 'angularfire2/firestore';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
 import { Product } from '../tools/Product'
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { ViewportScroller } from '@angular/common'
+import { ViewportScroller } from '@angular/common';
+
+interface Category {
+  title: string;
+  imgUrl:string;
+  keyWord:string;
+  displayOrder:number;
+}
 
 @Component({
   selector: 'app-edit-product',
@@ -11,6 +18,16 @@ import { ViewportScroller } from '@angular/common'
   styleUrls: ['./edit-product.component.css']
 })
 export class EditProductComponent implements OnInit {
+
+  categories: Observable<Category[]>;
+  private categoriesCol: AngularFirestoreCollection<Category>;
+  cdir = "CATEGORIES";
+
+  sk:string;
+  onoff:boolean;
+
+
+
   dir="PRODUCTS";
   products:any;
   doc:any;
@@ -19,6 +36,34 @@ export class EditProductComponent implements OnInit {
 
   ngOnInit() {
     //this.products = this.loadPrd(this.sta)
+    this.categoriesCol = this.db.collection<Category>(this.cdir, ref =>{
+      return ref.orderBy('displayOrder')
+    });
+    this.categories = this.categoriesCol.valueChanges();
+  }
+
+  myload(){
+    console.log("MMMM???")
+    this.products = this.loadwith()
+  }
+
+  loadwith(){
+    return this.db.collection(this.dir, ref=>{
+      return ref.where('status','==',this.onoff)
+      .where('keyword','==',this.sk)
+      //.orderBy("sales_count","desc")
+      //.orderBy("score", "desc")
+      //.orderBy("comment_count", "desc")
+      .limit(1)
+    }).snapshotChanges().pipe(map(actions=>{
+      return actions.map(a=>{
+        const data = a.payload.doc.data() as Product;
+        const id = a.payload.doc.id;
+        this.doc = a.payload.doc;
+        return {id,data};
+      })
+    }))
+
   }
 
   loadSta(s:boolean){
@@ -28,9 +73,9 @@ export class EditProductComponent implements OnInit {
   loadPrd(s:boolean){
     return this.db.collection(this.dir, ref=>{
       return ref.where('status','==',s)
-      .orderBy("sales_count","desc")
-      .orderBy("score", "desc")
-      .orderBy("comment_count", "desc")
+      //.orderBy("sales_count","desc")
+      //.orderBy("score", "desc")
+     // .orderBy("comment_count", "desc")
       .limit(1)
     }).snapshotChanges().pipe(map(actions=>{
       return actions.map(a=>{
@@ -72,6 +117,13 @@ export class EditProductComponent implements OnInit {
 
   }
 
+  editTinfo(key:string,sku){
+    this.db.doc(this.dir + '/' + key).update({trade_info:JSON.parse(sku)})
+    //console.log("turn on" + key)
+    //console.log("turn KK" + sku)
+
+  }
+
   editDetail(key:string,detail){
     this.db.doc(this.dir + '/' + key).update({detail:JSON.parse(detail)})
   }
@@ -96,6 +148,10 @@ export class EditProductComponent implements OnInit {
 
   resetKeyword(kw:string,key:string){
     this.db.doc(this.dir + '/' + key).update({keyword:kw})
+  }
+
+  resetPrice(pr:string,key:string){
+    this.db.doc(this.dir + '/' + key).update({price:pr})
   }
 
   delprd(key:string){
