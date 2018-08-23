6263bf41fcf9ee80b290f17fb630881e043fa164
singtolee <liqiang0618@outlook.com>
2018-08-23 17:03:33 +0700

    popular links

--- src/app/links-manager/links-manager.component.ts
+++ src/app/links-manager/links-manager.component.ts
@@ -1,88 +1,53 @@
-import { Component, OnInit } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
-import { AngularFireStorage, AngularFireUploadTask } from 'angularfire2/storage';
-import { Observable } from 'rxjs';
-import { finalize } from 'rxjs/operators';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { map } from 'rxjs/operators';
 
-
-interface TopLevelLink {
-  title: string;
+interface Link {
   url: string;
-  imgUrl:string;
-  clickCount:number;
+  keyword:string;
+  order:number;
 }
 @Component({
   selector: 'app-links-manager',
   templateUrl: './links-manager.component.html',
   styleUrls: ['./links-manager.component.css']
 })
-export class LinksManagerComponent implements OnInit {
+export class LinksManagerComponent implements OnInit, OnDestroy {
 
-  task: AngularFireUploadTask;
-  percentage: Observable<number>;
-  downloadURL: Observable<string>;
-  isHovering: boolean;
-  order;
-
-  mobileDir = "MOBILE-TOP-LINKS"
-  desktopDir = "TOP-LINKS"
-
-  topLevelLinks: Observable<TopLevelLink[]>;
-  private topLevelLinksCol: AngularFirestoreCollection<TopLevelLink>;
-  public topLevelLinksTitle: string = "";
-  public topLevelLinksUrl: string = "";
-  public imgUrl:string = "";
-  orderList = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-
-  constructor(private db: AngularFirestore, private storage: AngularFireStorage) {
-    this.topLevelLinksCol = db.collection<TopLevelLink>('TOP-LINKS');
-    this.topLevelLinks = this.topLevelLinksCol.valueChanges();
-  }
-
-  toggleHover(event:boolean){
-    this.isHovering = event;
-  }
-
-  startUpload(event:FileList){
-    const file = event.item(0)
-    if(file.type.split('/')[0] !== 'image'){
-      return
-    }
-
-    const path = `LINK-IMG/${new Date().getTime()}`;
-    const ref = this.storage.ref(path);
-    this.task = this.storage.upload(path,file);
-    this.percentage = this.task.percentageChanges();
-    this.task.snapshotChanges().pipe(
-      finalize(()=>this.downloadURL = ref.getDownloadURL())
-    ).subscribe();
-  }
-
-  isActive(snapshot){
-    return snapshot.state === 'running' && snapshot.bytesTransferred < snapshot.totalBytes
-  }
+  links:any;
+  dir = "LINKS";
+  url:string;
+  keyword:string;
+  order:number;
+  constructor(private db: AngularFirestore) {}
 
   ngOnInit() {
+    this.links = this.db.collection<Link>(this.dir).snapshotChanges().pipe(map(actions=>{
+      return actions.map(a=>{
+        const data = a.payload.doc.data() as Link;
+        const id = a.payload.doc.id;
+        return {id,data};
+      })
+    }))
   }
 
-  setUrl(r){
-    this.imgUrl = r;
-    console.log(this.imgUrl);
+  ngOnDestroy(){
+  }
+
+  del(key:string){
+    this.db.doc(this.dir + '/' + key).delete()
   }
 
   onSubmit(formData) {
     if (formData.valid) {
-      const data: TopLevelLink = {
-        title: formData.value.topLevelLinksTitle,
-        url: formData.value.topLevelLinksUrl,
-        imgUrl: this.imgUrl,
-        clickCount: 0,
+      const data = {
+        url:this.url,
+        keyword:this.keyword,
+        order:this.order
       }
-      this.db.collection(this.mobileDir).add(data).then((success) => {
-        this.topLevelLinksTitle = '';
-        this.topLevelLinksUrl = '';
-        this.imgUrl = '';
+      this.db.collection(this.dir).add(data).then((success) => {
+        this.url = ""
+        this.keyword = ""
       })
     }
   }
